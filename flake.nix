39fd26dca01c58d2160d39bafbc83cf28c907f72
wiyba <account@wiyba.org>
2026-03-18 15:31:31 +0300

    init xcli

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "xray subscription server";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+
+    env = pkgs.python3.withPackages (ps: with ps; [
+      fastapi
+      uvicorn
+      jinja2
+    ]);
+  in {
+    packages.${system}.default = pkgs.writeShellScriptBin "xcli" ''
+      exec ${env}/bin/python ${self}/main.py "$@"
+    '';
+  };
+}
