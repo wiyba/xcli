bec1967125489972636fe46d3412e82ad3ec2337
wiyba <account@wiyba.org>
2026-04-14 21:02:18 +0300

    add labels to uri

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import json, os
 
 HOSTS = [
-    {"name": "london", "label": "\U0001f1ec\U0001f1e7", "server": "london.wiyba.org", "port": 443},
-    {"name": "moscow", "label": "\U0001f1f7\U0001f1fa", "server": "moscow.wiyba.org", "port": 443},
-    {"name": "relay", "label": "\U0001f3f3\ufe0f", "server": "158.160.216.59", "port": 443},
+    {"name": "london", "label": "\U0001f1ec\U0001f1e7 london", "server": "london.wiyba.org", "port": 443},
+    {"name": "moscow", "label": "\U0001f1f7\U0001f1fa moscow", "server": "moscow.wiyba.org", "port": 443},
+    {"name": "relay", "label": "\U0001f3f3\ufe0f relay", "server": "158.160.216.59", "port": 443},
 ]
 
 USERS_FILE = os.environ.get("XCLI_USERS_FILE", "/run/secrets/xcli-users")
