bb609272f1f40f29225164567306568683484d63
wiyba <account@wiyba.org>
2026-04-16 00:18:20 +0300

    change users path

--- config.py
+++ config.py
@@ -1,21 +1,37 @@
-import json, os
+import json
+import os
 
 HOSTS = [
-    {"name": "london", "label": "\U0001f1ec\U0001f1e7 london", "server": "london.wiyba.org", "port": 443},
-    {"name": "moscow", "label": "\U0001f1f7\U0001f1fa moscow", "server": "moscow.wiyba.org", "port": 443},
+    {
+        "name": "london",
+        "label": "\U0001f1ec\U0001f1e7 london",
+        "server": "london.wiyba.org",
+        "port": 443,
+    },
+    {
+        "name": "moscow",
+        "label": "\U0001f1f7\U0001f1fa moscow",
+        "server": "moscow.wiyba.org",
+        "port": 443,
+    },
     {"name": "relay", "label": "relay", "server": "158.160.216.59", "port": 443},
 ]
 
-USERS_FILE = os.environ.get("XCLI_USERS_FILE", "/run/secrets/xcli-users")
+USERS_FILE = os.environ.get("XCLI_USERS_FILE", "/run/secrets/vless-users")
 
 
 def load_users():
-    return [{"name": u["email"], "uuid": u["id"], "sid": u["id"][:8]} for u in json.load(open(USERS_FILE))]
+    return [
+        {"name": u["email"], "uuid": u["id"], "sid": u["id"][:8]}
+        for u in json.load(open(USERS_FILE))
+    ]
 
 
 def reality():
     return {
-        "public_key": os.environ.get("XCLI_PUBLIC_KEY", "u-2Rr_En_Jx0agQKMG7DlwlLPus2hPLBPMXlOM_-lVU"),
+        "public_key": os.environ.get(
+            "XCLI_PUBLIC_KEY", "u-2Rr_En_Jx0agQKMG7DlwlLPus2hPLBPMXlOM_-lVU"
+        ),
         "short_id": os.environ.get("XCLI_SHORT_ID", "4ba9b78acaa91b44"),
         "sni": os.environ.get("XCLI_SNI", "yandex.ru"),
     }
